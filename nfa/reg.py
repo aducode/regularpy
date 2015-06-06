ad34e11e91f07be8b0702b6f3af2c52d0c5ba075
Issac <aducode@126.com>
2015-06-06 19:03:15 +0800

    test dfa

---
+++ nfa/reg.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+from graph import build_nfa
+from graph import nfa2dfa
+
+class Pattern(object):
+    def __init__(self, pattern):
+        self.start, self.ends, self.edges = nfa2dfa(*build_nfa(pattern))
+
+    def group(self, text, pos=0, endpos=None):
+        """
+        匹配正则
+        :param text: 要匹配的字符串
+        :param pos: 开始匹配的位置
+        :param endpos: 结束匹配的位置
+        :return
+        """
+        #import pdb
+        #pdb.set_trace()
+        text = text[pos:endpos if endpos is not None else  len(text)] if text else None
+        if text:
+            start = 0
+            end = -1 if self.start not in self.ends else 0 # -1 表示还没找到
+            i = 0
+            current = self.start
+            while i<len(text):
+                alpha = text[i]
+                try:
+                    next, _ = current.next(alpha).next()
+                except StopIteration:
+                    if end != -1:
+                        # 之前有过匹配
+                        if start<end:
+                            yield text[start:end]
+                        i = start = end
+                        end = -1
+                    else:
+                        # 之前没有过匹配
+                        i += 1
+                        start = i
+                    current = self.start
+                else:
+                    i += 1
+                    current = next
+                    if current in self.ends:
+                        end = i
+            if start<end:
+                yield text[start:end]
+
+def compile(pattern):
+    return Pattern(pattern)
--- nfa/test.py
+++ nfa/test.py
@@ -4,6 +4,7 @@ import os
 from graph import write2dot
 from graph import nfa2dfa
 from graph import build_nfa
+from reg import compile
 
 if __name__ == '__main__':
     i = 0
@@ -11,7 +12,7 @@ if __name__ == '__main__':
         pattern = raw_input('input pattern:\n')
         if pattern == '/quit' or pattern=='/q':
             break
-        start, end, edge_set = build_nfa(pattern)
+        #start, end, edge_set = build_nfa(pattern)
         if not os.path.exists('digraphs'):
             os.mkdir('digraphs')
         #with open('digraphs/nfa%d.dot'%i, 'w') as dot:
@@ -23,17 +24,24 @@ if __name__ == '__main__':
         #    dot.write('}')
         #os.system('dot2png.bat %d %s'% (i,'nfa'))
         #start, end, edge_set = compress_nfa(start, end, edge_set)
-        start, end, edge_set = nfa2dfa(start, end, edge_set)
+        #start, end, edge_set = nfa2dfa(start, end, edge_set)
+        p = compile(pattern)
         with open('digraphs/dfa%d.dot'%i, 'w') as dot:
             dot.write('digraph G{')
             dot.write('A[shape=box,label="%s"]' % pattern)
-            write2dot(edge_set, dot)
-            for _e in end:
+            write2dot(p.edges, dot)
+            for _e in p.ends:
                 dot.write('%s[color=red,peripheries=2]' % _e.id)
-            if start not in end:
-                dot.write('%s[color=blue]' % start.id)
+            if p.start not in p.ends:
+                dot.write('%s[color=blue]' % p.start.id)
             else:
-                dot.write('%s[color=blue,peripheries=2]' % start.id)
+                dot.write('%s[color=blue,peripheries=2]' % p.start.id)
             dot.write('}')
         os.system('dot2png.bat %d %s'% (i,'dfa'))
         i += 1
+        while True:
+            text = raw_input('input text:\n')
+            if text == '/q' or text == '/quit':
+                break
+            for tmp in p.group(text):
+                print '>\t', tmp
